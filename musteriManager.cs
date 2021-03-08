d107909e8089ae6513ae4a6b9cfb512aa56377d5
Ömer Elibol <53452104+cooller458@users.noreply.github.com>
2021-03-08 14:44:08 +0300

    Add files via upload

---
+++ musteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class musteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarı ile eklendi! : " + musteri.Adi  );
+
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri listelendi."+ musteri1);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri kaydı silindi."+ musteri.Adi);
+        }
+    }
+}
